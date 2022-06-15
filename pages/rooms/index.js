6fcd92a9550b33d9851f6d87f1530f187ae5226c
andreocunha <andreoliveiracunha20@gmail.com>
2022-06-14 23:12:51 -0300

    edicao dos quartos

--- pages/rooms/index.js
+++ pages/rooms/index.js
@@ -7,14 +7,14 @@ export default function Rooms(props) {
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       {data?.map((room) => (
-        <a href='#'>
-        <Link href="/room/[quarto]" as={`/room/${room?.rooms?.slug}`} key={room?.rooms?.nome_quarto}>
-          <div style={{ borderStyle: 'solid', margin: 10 }}>
-            <h2>{room?.rooms?.nome_quarto}</h2>
-            <p>{room?.rooms?.descricao_quarto}</p>
-            <img src={room?.rooms?.imagem_quarto} alt={room?.rooms?.nome_quarto} width={150} />
-          </div>
-        </Link>
+        <a href='#' key={room?.rooms?.nome_quarto}>
+          <Link href="/room/[quarto]" as={`/room/${room?.rooms?.slug}`}>
+            <div style={{ borderStyle: 'solid', margin: 10 }}>
+              <h2>{room?.rooms?.nome_quarto}</h2>
+              <p>{room?.rooms?.descricao_quarto}</p>
+              <img src={room?.rooms?.imagem_quarto} alt={room?.rooms?.nome_quarto} width={150} />
+            </div>
+          </Link>
         </a>
       ))}
     </div>
