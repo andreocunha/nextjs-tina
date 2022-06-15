92aa7622e9db63a28fbf7ba086e3831b7c3fccb2
andreocunha <andreoliveiracunha20@gmail.com>
2022-06-14 23:20:28 -0300

    edicao dos quartos

--- pages/room/[quarto].js
+++ pages/room/[quarto].js
@@ -5,22 +5,24 @@ import { useTina } from 'tinacms/dist/edit-state'
 export default function Room(props){
   const { data } = useTina({
     query: `{
-      rooms(relativePath: "${props.slug}"){
+      rooms(relativePath: "${props?.slug}"){
         nome_quarto,
         descricao_quarto,
         imagem_quarto
       }
     }`,
     variables: {},
-    data: props.data? props.data : {},
+    data: props?.data? props?.data : {},
   })
+
+  console.log(data)
   
   return(
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
       <h1>Quarto</h1>
-      <h1>{data.rooms.nome_quarto}</h1>
-      <p>{data.rooms.descricao_quarto}</p>
-      <img src={data.rooms.imagem_quarto} alt={data.rooms.nome_quarto} width={150} />
+      <h1>{data?.rooms.nome_quarto}</h1>
+      <p>{data?.rooms.descricao_quarto}</p>
+      <img src={data?.rooms.imagem_quarto} alt={data?.rooms.nome_quarto} width={150} />
     </div>
   );
 }
