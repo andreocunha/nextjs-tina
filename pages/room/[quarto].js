b6ddbfb6f4f67a99dbde4517058400b4fd0603c7
andreocunha <andreoliveiracunha20@gmail.com>
2022-06-15 19:50:36 -0300

    teste

--- pages/room/[quarto].js
+++ pages/room/[quarto].js
@@ -1,8 +1,8 @@
-import { getData } from "../../lib/api";
+import { staticRequest } from 'tinacms';
 import { useTina } from 'tinacms/dist/edit-state'
 
 
-export default function Room(props){
+export default function Room(props) {
   const { data } = useTina({
     query: `{
       rooms(relativePath: "${props?.slug}"){
@@ -15,7 +15,7 @@ export default function Room(props){
     data: props?.data,
   })
 
-  return(
+  return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
       <h1>Quarto</h1>
       <h1>{data?.rooms?.nome_quarto}</h1>
@@ -25,14 +25,49 @@ export default function Room(props){
   );
 }
 
+// export const getServerSideProps = async (context) => {
+//   const quarto = context.query.quarto;
+//   const data = await getData("rooms", quarto, "nome_quarto, descricao_quarto, imagem_quarto");
+
+//   return {
+//     props: {
+//       data: data,
+//       slug: quarto,
+//     }
+//   }
+// }
+
 export const getServerSideProps = async (context) => {
   const quarto = context.query.quarto;
-  const data = await getData("rooms", quarto, "nome_quarto, descricao_quarto, imagem_quarto");
 
+  // se o quarto tiver .md, então é um arquivo markdown
+  if (quarto.indexOf(".md") > -1) {
+    let data = {}
+    try {
+      data = await staticRequest({
+        query: `{
+        rooms(relativePath: "${quarto}"){
+          nome_quarto,
+          descricao_quarto,
+          imagem_quarto
+        }
+      }`,
+        variables: {},
+      })
+    } catch (err) {
+      console.log(err)
+    }
+
+    return {
+      props: {
+        data,
+        slug: quarto,
+      },
+    }
+  }
   return {
     props: {
-      data: data,
-      slug: quarto,
-    }
+      data: {}
+    },
   }
 }
