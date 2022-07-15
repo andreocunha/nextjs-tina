ad8e8c33c15f1ee2256623b36e5e103bd2d3793d
andreocunha <andreoliveiracunha20@gmail.com>
2022-07-15 12:48:46 -0300

    experimentalData

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -70,7 +70,7 @@ export const tinaConfig = defineConfig({
     return pack.TinaCloudCloudinaryMediaStore;
   },
   cmsCallback: (cms) => {
-    cms.flags.set("experimentalData", false);
+    cms.flags.set("experimentalData", true);
     //  add your CMS callback code here (if you want)
 
     // The Route Mapper
